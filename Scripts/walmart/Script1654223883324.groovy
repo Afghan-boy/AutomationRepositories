fd1646c46ac160b5c02473e0868ed090e3b75aa1
Naeyl <noorullahnaeyl@gmail.com>
2022-06-02 23:53:03 -0400

    SmartBEar commit

---
+++ Scripts/walmart/Script1654223883324.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://smartbear.com/')
+
+WebUI.maximizeWindow()
+
+WebUI.click(findTestObject('Object Repository/smartBear/Page_Software Testing, Monitoring, Develope_b469a2/a_Products'))
+
+WebUI.click(findTestObject('Object Repository/smartBear/Page_Software Testing, Monitoring, Develope_b469a2/span_ReadyAPI'))
+
+WebUI.verifyElementText(findTestObject('Object Repository/smartBear/Page_The All-In-One Automated API Testing P_ebef69/img_Pactflow_brand-text align-self-center h_0facf9'), 
+    '')
+
+WebUI.closeBrowser()
+
